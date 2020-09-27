a86e9b7842b6388288709186e9e3eb7f677ff194
pit-ray <pit-ray@outlook.com>
2020-09-27 21:33:53 +0900

    fixed ctrlp matching

--- install.ps1
+++ install.ps1
@@ -1,6 +1,15 @@
-cp ./.vimrc ~
-cp ./dein.toml ~
+cp .vimrc ~
+cp dein.toml ~
 
-git clone https://github.com/w0ng/vim-hybrid.git
-cp ./vim-hybrid/colors/hybrid.vim ~/vimfiles/colors/
+
+If(Test-Path vim-hybrid){
+    cd vim-hybrid
+    git pull
+    cd ..
+}
+Else{
+    git clone https://github.com/w0ng/vim-hybrid.git
+}
+
+cp vim-hybrid/colors/hybrid.vim ~/vimfiles/colors/
 
